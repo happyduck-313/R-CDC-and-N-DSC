f084a5fae565b3e3b589c24090e00640cdc71de3
happyduck-313 <99734336+happyduck-313@users.noreply.github.com>
2025-09-04 22:11:54 +0800

    Update Calculate_Matrix_L.m

--- funs/Calculate_Matrix_L.m
+++ funs/Calculate_Matrix_L.m
@@ -1,5 +1,10 @@
 % Calculate normalized Laplacian matrix
-function L = Calculate_Matrix_L(W)
-    degreeMatrix = sum(W, 2);  
-    L = diag(degreeMatrix) - W; 
+function L_norm = Calculate_Matrix_L(W)
+    degree = sum(W, 2);  
+    
+    degree(degree == 0) = 1;
+    
+    D_neg_sqrt = diag(degree.^(-0.5));
+    
+    L_norm = eye(size(W)) - D_neg_sqrt * W * D_neg_sqrt;
 end
